e284a4b7bdb071ceb60b2555686667fc9e613e27
figueiredomarcelo <137330786+figueiredomarcelo@users.noreply.github.com>
2023-06-28 16:14:20 -0300

    template 1

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -325,7 +325,7 @@ if (typeof jQuery === 'undefined') {
   Carousel.TRANSITION_DURATION = 600
 
   Carousel.DEFAULTS = {
-    interval: 500,
+    interval: 0,
     pause: 'hover',
     wrap: true,
     keyboard: true
